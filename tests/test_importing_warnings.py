502ba2f6d949ae1e5536257e528cb51121c66e91
Rotem Yaari <vmalloc@gmail.com>
2017-03-07 15:17:39 +0200

    Add test asserting no warnings are emitted during module loading

---
+++ tests/test_importing_warnings.py
@@ -0,0 +1,8 @@
+from emport import import_file
+
+def test_importing_doesnt_emit_warnings(tmpdir, module_file_factory, recwarn):
+    import warnings
+    warnings.simplefilter('always')
+    filename, _ = module_file_factory(tmpdir)
+    module = import_file(filename)
+    assert len(recwarn) == 0
